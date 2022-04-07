3c2f347f489b266064110831c43b9987a4343e72
Catherine_Vuthi <vuthi.esther@gmail.com>
2022-04-07 21:00:03 +0300

    Function lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    let distanceInBlocks = (Math.abs(distance - 42));
+    return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(feet) {
+    let distanceInFeet = (Math.abs(feet - 42) * 264);
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distanceTravelled = (Math.abs(start - end) * 264);
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distInFt = (Math.abs(start - destination) * 264);
+    if (distInFt <= 400) {
+        return 0;
+    } else if (distInFt > 400 && distInFt <= 2000) {
+        return Math.abs(distInFt - 400) * 2;
+    } else if (distInFt > 2000 && distInFt <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
